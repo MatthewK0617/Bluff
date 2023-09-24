ee81c6a8d15f70525cb7aaef489b336eea89476b
MatthewK0617 <kimmatthew2022@gmail.com>
2023-09-24 14:33:25 -0700

    new envs

--- bluff-backend/config/db.js
+++ bluff-backend/config/db.js
@@ -1,36 +1,23 @@
+require('dotenv').config();
 
 const mysql = require('mysql')
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: process.env.PASSWORD,
-    database: "card_game",
-    port: 7999,
-    // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+    host: process.env.PASSWORD || "localhost",
+    user: process.env.PASSWORD || "root",
+    password: process.env.PASSWORD || "191090465",
+    database: process.env.DB_NAME || "card_game",
+    port: process.env.PORT || 7999,
 })
-
-// db.connect();
 
 db.connect((err) => {
     if (err) throw err;
     console.log("Connected!");
 });
 
-// db.end(); // wait for db to connect; takes around 1 min
-
 module.exports = db;
-
-
-// could create an init_data which is used to always update the settings data upon loading
-
-
-// OR, since im going to have to create a new table for each game, could create each time create new game is selected
-
-
 
 /**
  * to start up:
- * 
  * start up MySQL server
  * nodemon index.js
  * npm start
