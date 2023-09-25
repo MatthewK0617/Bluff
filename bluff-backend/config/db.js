87d62ab38d5a0fa7fde94b701289eea370720269
MatthewK0617 <kimmatthew2022@gmail.com>
2023-09-24 17:10:42 -0700

    testing

--- bluff-backend/config/db.js
+++ bluff-backend/config/db.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 
 const mysql = require('mysql')
-const db = mysql.createConnection({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "191090465",
-    database: process.env.DB_NAME || "card_game",
-    port: process.env.DB_PORT || 7999,
+const db = process.env.NODE_ENV !== 'production' ? mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+}) : mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "191090465",
+    database: "card_game",
+    port: 7999,
 })
+
 
 db.connect((err) => {
     if (err) throw err;
